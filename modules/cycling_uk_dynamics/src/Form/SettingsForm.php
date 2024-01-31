2979554f345edb40f7d98c978cb2eaf6897e802f
Dan Duke <dan@agile.coop>
2024-01-31 16:28:27 +0000

    #62671 First pass at the endpoint switcher.

--- modules/cycling_uk_dynamics/src/Form/SettingsForm.php
+++ modules/cycling_uk_dynamics/src/Form/SettingsForm.php
@@ -53,6 +53,22 @@ class SettingsForm extends ConfigFormBase {
       '#default_value' => $this->config('dynamics.settings')->get('endpoint'),
     ];
 
+    $form['application_dev_wrapper'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Dev settings'),
+    ];
+    $form['application_dev_wrapper']['endpoint_dev'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Endpoint for Dev'),
+      '#description' => $this->t("Used to execute custom queries the library we're using can't build"),
+      '#default_value' => $this->config('dynamics.settings')->get('endpoint_dev'),
+    ];
+    $form['application_dev_wrapper']['instance_uri_dev'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Instance URI for Dev'),
+      '#default_value' => $this->config('dynamics.settings')->get('instance_uri_dev'),
+    ];
+
     $form['entity_whitelist'] = [
       '#type' => 'textarea',
       '#title' => $this->t('Dynamics entity whitelist'),
@@ -71,6 +87,10 @@ class SettingsForm extends ConfigFormBase {
     if (!UrlHelper::isValid($form_state->getValue('instance_uri'), TRUE)) {
       $form_state->setErrorByName('instance_uri', $this->t('Instance URI must be a valid URI.'));
     }
+    // instance_uri must be valid URL.
+    if (!UrlHelper::isValid($form_state->getValue('instance_uri_dev'), TRUE)) {
+      $form_state->setErrorByName('instance_uri_dev', $this->t('Instance URI for dev must be a valid URI.'));
+    }
     // application_id must be in the form 6d05b2be-24c0-4eac-90b1-f72b78dd6d1a.
     if (!self::pregMatch('/^[a-f\d]{8}(-[a-f\d]{4}){3}-[a-f\d]{12}$/', $form_state->getValue('application_id'))) {
       $form_state->setErrorByName('application_id', $this->t('Application ID must be a valid GUID in the form abcdef01-2345-6789-abcd-ef0123456789.'));
@@ -83,6 +103,10 @@ class SettingsForm extends ConfigFormBase {
     if (!UrlHelper::isValid($form_state->getValue('endpoint'), TRUE)) {
       $form_state->setErrorByName('endpoint', $this->t('Endpoint must be a valid URI.'));
     }
+    // Endpoint must be valid URL.
+    if (!UrlHelper::isValid($form_state->getValue('endpoint_dev'), TRUE)) {
+      $form_state->setErrorByName('endpoint_dev', $this->t('Endpoint for dev must be a valid URI.'));
+    }
 
     parent::validateForm($form, $form_state);
   }
@@ -94,9 +118,11 @@ class SettingsForm extends ConfigFormBase {
     $config = $this->config('dynamics.settings');
     $config
       ->set('instance_uri', $form_state->getValue('instance_uri'))
+      ->set('instance_uri_dev', $form_state->getValue('instance_uri_dev'))
       ->set('application_id', $form_state->getValue('application_id'))
       ->set('application_secret', $form_state->getValue('application_secret'))
       ->set('endpoint', $form_state->getValue('endpoint'))
+      ->set('endpoint_dev', $form_state->getValue('endpoint_dev'))
       ->set('entity_whitelist', preg_split("/\r\n|\n|\r/", $form_state->getValue('entity_whitelist'), -1, \PREG_SPLIT_NO_EMPTY));
     $config->save();
 
